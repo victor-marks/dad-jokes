7ab9266aba8b47cfcf4a284cb60bf5e2dfe65631
Victor Marks <vjmarks@gmail.com>
2019-09-25 11:56:21 -0700

    created Joke component

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import Joke from './Joke';
 import axios from 'axios';
+import uuid from 'uuid/v4';
 import './JokeList.css';
 
 class JokeList extends Component {
@@ -13,6 +15,7 @@ class JokeList extends Component {
       jokes: []
     };
   }
+
   async componentDidMount() {
     // Load Jokes
     let jokes = [];
@@ -20,9 +23,17 @@ class JokeList extends Component {
       let res = await axios.get('https://icanhazdadjoke.com/', {
         headers: { Accept: 'application/json' }
       });
-      jokes.push(res.data.joke);
+      jokes.push({ id: uuid(), text: res.data.joke, votes: 0 });
     }
     this.setState({ jokes });
+  }
+
+  handleVote(id, delta) {
+    this.setState(st => ({
+      jokes: st.jokes.map(j =>
+        j.id === id ? { ...j, votes: j.votes + delta } : j
+      )
+    }));
   }
 
   render() {
@@ -38,7 +49,13 @@ class JokeList extends Component {
 
         <div className="JokeList-jokes">
           {this.state.jokes.map(j => (
-            <div>{j}</div>
+            <Joke
+              key={j.id}
+              votes={j.votes}
+              text={j.text}
+              upvote={() => this.handleVote(j.id, 1)}
+              downvote={() => this.handleVote(j.id, -1)}
+            />
           ))}
         </div>
       </div>
